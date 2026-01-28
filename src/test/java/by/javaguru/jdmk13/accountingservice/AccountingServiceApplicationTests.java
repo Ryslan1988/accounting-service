935dd5054a36014da888728c2e9c56b3cb022af8
1 <a1@MacBook-Pro-1.local>
2026-01-28 19:48:22 +0300

    Initial commit

---
+++ src/test/java/by/javaguru/jdmk13/accountingservice/AccountingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.javaguru.jdmk13.accountingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
