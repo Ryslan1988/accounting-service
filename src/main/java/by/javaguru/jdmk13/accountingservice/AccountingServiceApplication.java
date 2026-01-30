6516a5ecb637837bc59f5f58b8dbeea9dfd5a336
1 <a1@MacBook-Pro-1.local>
2026-01-30 20:06:58 +0300

    https://manage.javaguru.team/browse/JDMK13-24 fix eurica'

--- src/main/java/by/javaguru/jdmk13/accountingservice/AccountingServiceApplication.java
+++ src/main/java/by/javaguru/jdmk13/accountingservice/AccountingServiceApplication.java
@@ -2,8 +2,10 @@ package by.javaguru.jdmk13.accountingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountingServiceApplication {
 
     public static void main(String[] args) {
